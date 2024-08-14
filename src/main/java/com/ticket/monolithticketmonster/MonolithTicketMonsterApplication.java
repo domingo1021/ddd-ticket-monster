704a4ff2174a06118a6666d4a43b7c0cc50a26bf
domingo1021 <domingo.backend@gmail.com>
2024-08-14 16:21:51 +0800

    chore: init spring boot project

---
+++ src/main/java/com/ticket/monolithticketmonster/MonolithTicketMonsterApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.monolithticketmonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithTicketMonsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithTicketMonsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticket/monolithticketmonster/MonolithTicketMonsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.monolithticketmonster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithTicketMonsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
