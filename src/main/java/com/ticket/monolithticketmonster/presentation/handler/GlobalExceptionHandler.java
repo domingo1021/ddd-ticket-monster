39999d72c9e2ad47673923d7c8c044d237f462da
domingo1021 <domingo.backend@gmail.com>
2024-08-16 15:45:55 +0800

    refactor: remove unused dependency

--- src/main/java/com/ticket/monolithticketmonster/presentation/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ticket/monolithticketmonster/presentation/handler/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.ticket.monolithticketmonster.presentation.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
