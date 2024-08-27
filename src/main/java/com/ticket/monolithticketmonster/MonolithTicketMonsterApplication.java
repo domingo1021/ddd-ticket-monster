e1a8bb8be85612ebfa640192c8ea451234379ee3
domingo1021 <domingo.backend@gmail.com>
2024-08-27 18:40:46 +0800

    feat: add default setting for spring profile

--- src/main/java/com/ticket/monolithticketmonster/MonolithTicketMonsterApplication.java
+++ src/main/java/com/ticket/monolithticketmonster/MonolithTicketMonsterApplication.java
@@ -3,14 +3,18 @@ package com.ticket.monolithticketmonster;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class MonolithTicketMonsterApplication {
 
   public static void main(String[] args) {
-    // Load env before app start
-    Dotenv dotenv = Dotenv.configure().load();
-    dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    // Load .env file if SPRING_PROFILES_ACTIVE is not set (assuming it's the local environment)
+    String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
+    if (activeProfile == null || activeProfile.isEmpty()) {
+      Dotenv dotenv = Dotenv.configure().load();
+      dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
 
     SpringApplication.run(MonolithTicketMonsterApplication.class, args);
   }
